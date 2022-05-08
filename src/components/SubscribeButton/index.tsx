1b4439ffa31fc2bdc3846d479b9f8feb3997f46b
caiomartinsd98 <caio.martins@gira.app.br>
2022-05-07 23:35:44 -0300

    Feat: Pagement in Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,22 +1,46 @@
 import { useSession, signIn } from "next-auth/react";
+import { useState } from "react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
-
+import ReactLoading from "react-loading";
 interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
+    setLoading(true);
     if (!session) {
       signIn("github");
       return;
     }
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+      setLoading(false);
+    } catch (err) {
+      alert(err.message);
+      setLoading(false);
+    }
     //criação da checkout session https://stripe.com/docs/api/checkout/sessions
   };
   return (
-    <button type="button" className={styles.subscribeButton}>
-      Subscribe Now
+    <button
+      onClick={handleSubscribe}
+      type="button"
+      className={styles.subscribeButton}
+      disabled={loading ? true : false}
+    >
+      {loading ? (
+        <ReactLoading type="bubbles" color="#121214" />
+      ) : (
+        "Subscribe Now"
+      )}
     </button>
   );
 }
