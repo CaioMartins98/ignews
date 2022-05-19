a04cf4039ee2acb0f18bfd8d1d4f419820c7900d
Caio <caio.martins@gira.app.br>
2022-05-19 20:00:00 -0300

    Fix: return buffer

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -10,6 +10,8 @@ const buffer = async (readable: Readable) => {
   for await (const chunk of readable) {
     chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
   }
+
+  return Buffer.concat(chunks);
 };
 
 export const config = {
